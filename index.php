f5022ff1b0a0d517c9ee4c689e44db88731608ff
david17321 <davidoconnor61@gmail.com>
2026-02-08 07:53:03 -0800

    initial

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+$dir = $_GET['dir'] ?? '.';
+if (!is_dir($dir)) {
+    echo $dir . " is not a directory.";
+    die();
+}
+$imageDirs = scandir($dir);
+
+
+function imWidth($a, $b) {
+    $sizeA = getimagesize($a);
+    $sizeB = getimagesize($b);
+    if ($sizeA && $sizeB) {
+        return ($sizeA[0]) <=> ($sizeB[0] );
+    }
+    return 0;
+}   
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File Browser</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+<body>
+    <h1>File Browser</h1>
+    <ul>
+        <?php 
+        foreach ($imageDirs as $imageDir) {
+            /* If $file is a directory, list the file and each image file it contains. */
+            /* Skip the current and parent directory entries. */
+            if ($imageDir === '.' || $imageDir === '..') {
+                continue;
+            }
+            if (is_dir("$dir/$imageDir")) { 
+                $jjj = "$dir/$imageDir";
+                echo("<li><p>Directory " . $jjj . "</p>");
+        ?>                      
+                    <?php
+                    $subFiles = scandir("$jjj");
+                    foreach ($subFiles as $subFile) {
+                        if (is_file("$dir/$imageDir/$subFile") && preg_match('/\.(jpg|jpeg|png|gif)$/i', $subFile)) {
+                            echo "<p><span><a href=\"$dir/$imageDir/$subFile\">$subFile</a></span>";
+                            // list the dimensions of the image
+                            $imageSize = getimagesize("$dir/$imageDir/$subFile");
+                            if ($imageSize) {
+                                echo "<span>&nbsp;" . $imageSize[0] . "x" . $imageSize[1] . "</span>";
+                            }
+                            else {
+                                echo ("<span>File has no dimensions.</span>");
+                            }
+                            echo ("</p>");
+                        
+                        }
+                        else {
+                            unset($subFiles[array_search($subFile, $subFiles)]);
+                        }
+                    }
+                    usort($subFiles, 'imWidth');
+                    foreach ($subFiles as $s) {
+                        echo($s . "<br>");
+                    }
+                ?></li><?php
+            }
+        }
+    
+        ?>
+    </ul>
+</body></html>
+
+
