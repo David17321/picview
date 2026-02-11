35a4a9d9293e09d6edcc21b45ab82d855a1436c6
david17321 <davidoconnor61@gmail.com>
2026-02-11 10:38:01 -0800

    Formatting

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+// To Do:  Use Masonry JS Library to arrange pictures.
+$devVersion = true;
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+
+if ($devVersion) {
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+}
 
 $maindir = $_GET['dir'] ?? '.';
 if ($maindir === false || !is_dir($maindir)) {
@@ -35,39 +40,20 @@ function getImages($directory) {
 ///////////////////////////////////////////////////////////////////////////////
 ?>
 
-
-
-
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Image Browser</title>
-
+    
     <link 
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
         rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" 
         crossorigin="anonymous">
 
-    <style>
-        body {
-            color: white;
-            background-color: #291b53;
-        }
-        li {
-            margin-top: 1rem;
-            margin-bottom: 1rem;
-        }
-        details>p {
-            margin-bottom: 0rem;
-            margin-left: 2rem;
-        }
-        p>span {
-            margin-right: 1rem;
-            color: #ffff88;
-        }
-    </style>
+
+    <link rel="stylesheet" type="text/css" href="picview.css">
 
     <script
         src="https://code.jquery.com/jquery-4.0.0.min.js"
@@ -75,26 +61,31 @@ function getImages($directory) {
         crossorigin="anonymous"
         async>
     </script>
-
+<!--
     <script 
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" 
         crossorigin="anonymous"
         async>
     </script>
+-->
+
+    <script
+        src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js">
+    </script>
 
 </head>
 <body>
-    <h1>Image Browser</h1>
+    <h1>Image Browser<?php if($devVersion) echo("  <em>[DEV VERSION&mdash;DO NOT UPLOAD]</em>"); ?></h1>
     <?php
     echo("<ul>");
     for ($i = 0; $i < sizeof($imageDirs); $i++) {
         echo("<li>\n");
-        echo("<span>". $imageDirs[$i] . "\n");
+        echo("<span>". $imageDirs[$i] . "</span>\n");
         echo("<details>\n");
         foreach($allImages[$i] as $img) {  
             $imSize =  getimagesize($img);
-            echo("<p><span>" . $img . "</span><span>" . $imSize[0] . "x" . $imSize[1] . "</span></p>\n");
+            echo("<p><span>" . $img . "</span><span>" . $imSize[0] . "&times;" . $imSize[1] . "</span></p>\n");
         }
         echo("</details>\n");
         echo("</li>\n");
