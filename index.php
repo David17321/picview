def13904effddfa20fc0491bdc335fa5305b9afd
david17321 <davidoconnor61@gmail.com>
2026-02-13 08:09:05 -0800

    lots of headway on responsiveness but need to make it work on smart TVs

--- index.php
+++ index.php
@@ -57,6 +57,13 @@ foreach ($imageDirs as $imageDir) {
     }
 }
 
+$htmlFiles = glob($maindir . '/*.html');
+if (!$htmlFiles) {
+    $headingText = "<h1>Image Gallery</h1>";
+} else {
+    $headingText = file_get_contents($htmlFiles[0]);
+}
+
 $allImages = [];
 foreach ($imageDirs as $imageDir) {
     $allImages[] = getImages($imageDir);  
@@ -88,14 +95,21 @@ function getImages($directory) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Image Browser</title>
     
-    <link 
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
-        rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" 
-        crossorigin="anonymous">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+        crossorigin="anonymous">   
 
     <link rel="stylesheet" type="text/css" href="picview.css">
+    <link rel="stylesheet" type="text/css" href="masonry_styling.css">
 
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"
+        async>
+    </script>   
+    
     <script
         src="https://code.jquery.com/jquery-4.0.0.min.js"
         integrity="sha256-OaVG6prZf4v69dPg6PhVattBXkcOWQB62pdZ3ORyrao="
@@ -120,70 +134,19 @@ function getImages($directory) {
         
     </script>
 
-    <style>
-        * { box-sizing: border-box; }
-
-body { font-family: sans-serif; }
-
-/* ---- grid ---- */
-
-.grid {
-  /*background: #222;*/
-  max-width: 1200px;
-  margin-left: 1em;
-}
-
-/* clearfix */
-.grid:after {
-  content: '';
-  display: block;
-  clear: both;
-}
-
-/* ---- grid-item ---- */
-
-.grid-sizer,
-.grid-item {
-  width: 33%;
-}
-
-.grid-item {
-  height: 10vh;
-  float: left;
-  /*
-  background: #222;
-  border: 2px solid #222;
-  border-color: #222;*/
-}
-
-.grid-item--width1 { width: 16.667%; }
-.grid-item--width2 { width: 33.333%; }
-.grid-item--width3 { width: 50%; }
-
-.grid-item--height1 { height: 20vh; }
-.grid-item--height2 { height: 40vh; }
-
-.grid-item img {
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
-    padding: 1vh;
-    /*border: 1px solid white; */
-}
-</style>
-
 </head>
 <body>
     <div class=container>
-    <h1 style="margin: 1em;">Image Browser<?php if($devVersion) echo("  <em>[DEV VERSION&mdash;DO NOT UPLOAD]</em>"); ?></h1>
     <?php
-    
+    echo($headingText); 
+    if($devVersion) echo("<h1><em>[DEV VERSION&mdash;DO NOT UPLOAD]</em></h1>"); 
     echo("<div class=grid>");
     echo("<div class=\"grid-sizer\"></div>");
     shuffle($imageDirObjects);
     for ($i = 0; $i < sizeof($imageDirObjects); $i++){
         $ar = $imageDirObjects[$i]->aspectRatio;
 
+        // Allocate grid space depending on the image's aspect ratio
         if ($ar > 2.0) {
              $shapeClass =  "grid-item--width3 grid-item--height1";
         }
