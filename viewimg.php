db7774fd4c7d66fa8a3b2c06ad5e788849dd7ea4
David17321 <119080839+David17321@users.noreply.github.com>
2026-02-19 07:41:52 -0800

    almost

--- viewimg.php
+++ viewimg.php
@@ -1,12 +1,6 @@
 <?php
 
-$devVersion = true;
-
-if ($devVersion) {
-    ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-    error_reporting(E_ALL);
-}
+include 'enable_warnings.php';
 
 $fileName = $_GET['file'];
 
